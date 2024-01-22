004630ad15491a61a2042a9e076038847a629d2b
Knyazev Nikita <nk8222138@gmail.com>
2024-01-22 14:22:46 +0300

    Добавьте файлы проекта.

---
+++ Math_Fun/math.h
@@ -0,0 +1,2 @@
+#pragma once
+int math(int a, int b, int c);
---
+++ Math_Fun/source.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include "math.h"
+#include "windows.h"
+
+using  std::cout;
+using  std::cin;
+
+
+int main()
+{
+   SetConsoleCP(1251);
+   SetConsoleOutputCP(1251);
+   int a, b, c;
+   cout << "Введите первое число: ";
+   cin >> a;
+   cout << "\nВведите второе число: ";
+   cin >> b;
+   cout << "Выберите операцию (1 - сложение, 2 вычитание, 3 - умножение, 4 - деление, 5 - возведение в степень): ";
+   cin >> c;
+
+   switch (c) {
+   case 1: cout << a << " плюс " << b << " равно " << math(a, b, c); break;
+   case 2: cout << a << " минус " << b << " равно " << math(a, b, c); break;
+   case 3: cout << a << " умножить " << b << " равно " << math(a, b, c); break;
+   case 4: cout << a << " деление " << b << " равно " << math(a, b, c); break;
+   case 5: cout << a << " в степени " << b << " равно " << math(a, b, c); break;
+   default: break;
+   }
+}
